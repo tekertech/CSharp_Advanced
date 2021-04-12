422fd918357a6e5d0ca41e2f2234a271373c87ae
Mehmet Teker <teker.mehmed@gmail.com>
2021-04-13 00:31:44 +0300

    RAM(STACK/HEAP);  Value Type(IsValueType) - Primitive Type (IsPrimitive), Literal Düzenleme(Alt tire (_) C# 7.0), Default Literal C# 7.1

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -10,60 +10,59 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            #region İterasyon Nedir?, Dizi, ArrayList Koleksiyonlar, ArrayList ile Boxing - UnBoxing Durumları, List
+            #region RAM(STACK/HEAP);  Value Type(IsValueType) - Primitive Type (IsPrimitive), Literal Düzenleme(Alt tire (_) C# 7.0), Default Literal C# 7.1
             /*
-             * Diziler ve Koleksiyonlar aynı amacı görmesine rağmen dizideki bazı eksiklikleri tamamlar koleksiyonlar.
-             *   ---  Dizilerde tanımlama yaparken eleman sayısının bildirilmesi zorunluluğu bir sınırlılıktır.
-             *   ---  Diziler tanımlandığında kullansakta kullanmasakta bellekte belirtilen eleman sayısı kadar alan tahsis edilecektir.
-             *   ---  Dizilerde eleman sayısının başta belirlenmesi durumunda, ihtiyaca binaen daha fazla değer atamak istediğimizde bu değerleri atayamacağımızdan ve dizinin
-             *        aralığını genişletemeyeceğimizden dolayı bu durum bir sınırlılık olarak karşımıza çıkmaktadır. 
-             * ArrayList :
-             *       --  Verilen datayı boxing işlemine tabi tutar.
-             *       --  İçerde herhangi bir datayı talep ettiğimizde o veri object olarak gelecektir. Dolayısıyla kendi türünde işlem yapabilmek için unboxing işlemine tabi tutmamız gerekir.
-             * İterasyon  :
-             *       --  Veri kümelerini incelerken döngülerde faydalanırız.
-             *       İterasyon: Tekerrür, tekrarlama, yineleme ve mükerrer icrâ, ardışık işlem anlamlarına gelen iterasyon (iteration), programlamada bir dizi işlemin döngüler kullanarak yazılmasıdır. (Halk ağzıyla ittirmek :))
-             *       foreach bu yapıda tercih edilir. 
-             *İterasyon vs Döngü :
-             *      Foreach(iterasyon) bir döngü değildir.  Tüm elemanlar bitene kadar devam eder. (bir mantık yoktur veya kombinasyon yoktur.)
-             *      Döngüler : bir mantığa göre, kombinasyoma göre işlem yapar.
+             * C# programlama dilinde RAM de veri tutabilmek depolayabilmek için tanımlanacak olan değişkenin türü/veri türü bildirilmelidir.
+             * Bir değişkenle RAM'de alan tahsisinde bulunulduğunda buna Deger türlü değişken diyoruz. Yani tuttuğu deger bir normal deger olan değişkenlere deger türlü denmektedir.
+             * Değer Türlü değişkenler sade ve sadece br değeri tutan değişkenlerdir. Yani adınız soyadınız yaşınız gibi.
+             * Primitive Type : En ilkel türdür; byte bir primitive türdür fakat decimal primitive değildir çünkü byte lardan meydana gelmiştir. ikiside value type dir.
+             * Value Type , Primitive Type leri kapsar.
+             * Değişken tanımlarken RAM'de tutulacak veriye uygun bir alan tahsisinde bulunabilmek için ilgili değişkenin turunden hareket edilir.
+             * RAM de bir türden tanımlanmış alana farklı bir türde deger atayamayız.
+             * 
+             * * ***********************************************************************************************************************TIP*********************
+             * *Tür*******Açıklama ve Bellek Alanı***************Max-Min aralığı                                                                               *
+             * *************************************************************************************************************************TIP*********************
+             * MANTIKSAL  * bool     *  Doğru veya yanlış (1 Bit)      *        0-1 (True- False)                                                 * MANTIKSAL  *
+             * METINSEL   * char     *  Karakterler(16 Bit)            *        16 Bit Unicode                                                    * METINSEL   *
+             * SAYISAL    * sbyte    *  İşaretli Tam Sayı(8 Bit)       *       -128 ile 127 arası                                                 * SAYISAL    *
+             * SAYISAL    * byte     *  İşaretsiz Tam Sayı(8 Bit)      *        0 ile 255 arası                                                   * SAYISAL    *
+             * SAYISAL    * short    *  İşaretli Tam Sayı(16 Bit)      *        -32.768 ile 32.767                                                * SAYISAL    *
+             * SAYISAL    * ushort   *  İşaretsiz Tam Sayı(16 Bit)     *        0 ile 65.535                                                      * SAYISAL    *
+             * SAYISAL    * int      *  İşaretli Tam Sayı(32 Bit)      *        -2.147.483.468 ile 2.147.483.647 arası                            * SAYISAL    *
+             * SAYISAL    * uint     *  İşaretsiz Tam Sayı(32 Bit)     *        0 ile 4.294.967.295 arası                                         * SAYISAL    *
+             * SAYISAL    * long     *  İşaretli Tam Sayı(64 Bit)      *        -9.223.372.036.854.775.808 ile 9.223.372.036.854.775.808 arası    * SAYISAL    *
+             * SAYISAL    * ulong    *  İşaretsiz Tam Sayı(64 Bit)     *         0 ile 18.446.744.073.709.551.615 arası                           * SAYISAL    *
+             * ONDALIK    * float    *  Tek kayan Sayı(32 Bit)         *                                                                          * ONDALIK    *
+             * ONDALIK    * double   *  Çift Kayan Sayı (64 Bit)       *                                                                          * ONDALIK    *
+             * ONDALIK    * decimal  *  Ondalık Sayı(128 Bit)          *                                                                          * ONDALIK    *
+             * ***************************************************************************************************************************TIP*******************
+             *  Değişkenler RAM ın STACK bölgesinde tutulur. 
+             *  Kompleks sayısal ifadeleri _ (Alt tire) ile düzenlememizi sağlayan özelliktir. 10000000000 ->   10_000_000_000
+             *  Default Literal :  default keyword ü ile türün varsayılan değerini geri döndürür.
+             *                  string : null           bool : false
+             *                  sayisal: 0              char : ''
+             *                  Double : 0              byte : 0
+             *  Main içerisinde oluşturulan değişkenlerin ilk değerlerini manuel atmaya özen gösteriniz. Class larda default verilir otomatik olarak.
+             *  Default Literal C# 7.1 int x = default;
              */
-
-            int[] ages = new int[17]; // Kaç eleman olacağını belirttik.
-            ages[1] = 14;
-
-
-            ArrayList _ages = new ArrayList(); // Eleman sayısını belirtmedik.
-            _ages.Add(14);
-            _ages.Add("Mehmet");
-            _ages.Add(25);
-            _ages.Add(0.25);
-            _ages.Add(true);
-            _ages.Add('F');
-
-            int toplam = 0;
-            foreach (var item in _ages)
-            {
-                // Boxing - UnBoxing
-                //  _ages.Add(object value)   object olarak içeri alır biz int,string versekte
-
-                if (item is int number) {  //  Type Pattern işlemi ile int olanları aldık.
-                    toplam += number;
-                }
-                Console.WriteLine("Items :" + item);
-            }
-
-             // Tip alıp eklemek.            
-            List<int> vs = new List<int>()
-            {
-                14,25,36,87
-            };
-
             #endregion
+            Console.WriteLine("IsPrimitive : " + typeof(decimal).IsPrimitive + " IsValueType : " + typeof(decimal).IsValueType);
+            Console.WriteLine("IsPrimitive :" + typeof(byte).IsPrimitive + " IsValueType : " + typeof(byte).IsValueType);
+            Console.WriteLine("IsPrimitive :" + typeof(string).IsPrimitive + " IsValueType : " + typeof(string).IsValueType);
 
-            
+            //  Değişkenler RAM ın STACK bölgesinde tutulur. Run-Time'da alan tahsisinde bulunur.
+            int yas;
+            string name;
 
+            // Literal Düzenleme (C# 7.0)
+            int number = 1_000_000;
 
+            Console.WriteLine("Number : " + number);
+            Console.WriteLine(default(bool) + " " + default(char) + " " + default(byte));
+            Console.WriteLine(default(short) + " " + default(int) + " " + default(long));
+            Console.WriteLine(default(float) + " " + default(double) + " "+ default(decimal));
+            Console.WriteLine(default(string) + " " + default(object));
 
             Console.ReadLine();
         }
